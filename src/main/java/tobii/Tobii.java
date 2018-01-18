13731a48422d27d5fe073de2d2fbed719f3b2a08
coylz <contact.coyl@gmail.com>
2018-01-18 14:43:58 +0100

    Refact

---
+++ src/main/java/tobii/Tobii.java
@@ -0,0 +1,57 @@
+package tobii;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Tobii
+{
+	public static float[] gazePosition()
+	{
+		if (instance == null)
+		{
+			instance = new Tobii();
+		}
+		return instance.jniGazePosition();
+	}
+
+	private static Tobii instance = null;
+
+	private Tobii()
+	{
+		loadLibraryFromResources("/lib/tobii/x64/tobii_stream_engine.dll");
+		loadLibraryFromResources("/lib/tobii/x64/tobii_jni_stream_engine.dll");
+		int code = jniInit();
+		System.out.println("Tobii init code error " + code);
+	}
+
+	public static void loadLibraryFromResources(String filePath)
+	{
+		try
+		{
+			InputStream in = Tobii.class.getResourceAsStream(filePath);
+			byte[] buffer = new byte[1024];
+			int read = -1;
+			String tmpDir = System.getProperty("java.io.tmpdir");
+			File tmpFile = new File(tmpDir, filePath);
+			tmpFile.getParentFile().mkdirs();
+			FileOutputStream fos = new FileOutputStream(tmpFile);
+			while((read = in.read(buffer)) != -1) 
+			{
+				fos.write(buffer, 0, read);
+			}
+			fos.close();
+			in.close();
+			System.load(tmpFile.getAbsolutePath());
+		}
+		catch (IOException e)
+		{
+			throw new RuntimeException(e);
+		}
+	}
+
+	private native int jniInit();
+
+	private native float[] jniGazePosition();
+}
