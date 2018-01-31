ad91e7387dbb90d21586355e87d12ec2dbae8511
Loïc Vial <contact.coyl@gmail.com>
2018-01-31 16:13:58 +0100

    Refact, copy Dlls into %LocalAppData%\TobiiStreamEngineForJava\lib\tobii\x64

--- src/main/java/tobii/Tobii.java
+++ src/main/java/tobii/Tobii.java
@@ -7,87 +7,86 @@ import java.nio.file.StandardCopyOption;
 
 public class Tobii
 {
-	public static boolean verbose = true;
-	
-	public static float[] gazePosition()
-	{
-		if (instance == null)
-		{
-			instance = new Tobii();
-		}
-		return instance.jniGazePosition();
-	}
+    public static boolean verbose = true;
 
-	private static Tobii instance = null;
+    public static String dataDirectoryPath = "%LocalAppData%\\TobiiStreamEngineForJava";
 
-	private Tobii()
-	{
-		loadNeededLibraries();
-		int code = jniInit();
-		printIfVerbose("Init code error " + code);
-	}
-	
-	private static void loadNeededLibraries()
-	{
-		printIfVerbose("Loading needed libraries");
-		String tmpDir = System.getProperty("java.io.tmpdir");
-		printIfVerbose("Using tmpdir " + tmpDir);
-		
-//		copyResourceIntoDir("/lib/tobii/x64/vcruntime140.dll", tmpDir);
-//		copyResourceIntoDir("/lib/tobii/x64/msvcp140.dll", tmpDir);
-//		copyResourceIntoDir("/lib/tobii/x64/concrt140.dll", tmpDir);
-//		copyResourceIntoDir("/lib/tobii/x64/vccorlib140.dll", tmpDir);
-		copyResourceIntoDir("/lib/tobii/x64/tobii_stream_engine.dll", tmpDir);
-		copyResourceIntoDir("/lib/tobii/x64/tobii_jni_stream_engine.dll", tmpDir);
-		
-//		loadLibrary(tmpDir, "/lib/tobii/x64/vcruntime140.dll");
-//		loadLibrary(tmpDir, "/lib/tobii/x64/msvcp140.dll");
-//		loadLibrary(tmpDir, "/lib/tobii/x64/concrt140.dll");
-//		loadLibrary(tmpDir, "/lib/tobii/x64/vccorlib140.dll");
-		loadLibrary(tmpDir, "/lib/tobii/x64/tobii_stream_engine.dll");
-		loadLibrary(tmpDir, "/lib/tobii/x64/tobii_jni_stream_engine.dll");
-	}
-	
-	private static void copyResourceIntoDir(String resourceFilePath, String dirPath)
-	{
-		try
-		{
-			printIfVerbose("Copying " + resourceFilePath + " into " + dirPath);
-			InputStream in = Tobii.class.getResourceAsStream(resourceFilePath);
-			File tmpFile = new File(dirPath, resourceFilePath);
-			tmpFile.getParentFile().mkdirs();
-			Files.copy(in, tmpFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
-			in.close();
-		}
-		catch (Exception e)
-		{
-			e.printStackTrace();
-		}
-	}
+    private static boolean loaded = false;
 
-	private static void loadLibrary(String dirPath, String filePath)
-	{
-		try
-		{
-			File tmpFile = new File(dirPath, filePath);
-			printIfVerbose("Loading library " + tmpFile.getAbsolutePath());
-			System.load(tmpFile.getAbsolutePath());
-		}
-		catch (Exception e)
-		{
-			e.printStackTrace();
-		}
-	}
-	
-	private static void printIfVerbose(String what)
-	{
-		if (verbose)
-		{
-			System.out.println("Tobii: " + what);
-		}
-	}
+    private static boolean errorReported = false;
 
-	private native int jniInit();
+    public static float[] gazePosition()
+    {
+        try
+        {
+            loadIfNotLoaded();
+            return jniGazePosition();
+        }
+        catch (Throwable e)
+        {
+            if (!errorReported)
+            {
+                e.printStackTrace();
+                errorReported = true;
+            }
+            return new float[] { 0.5f, 0.5f };
+        }
+    }
 
-	private native float[] jniGazePosition();
+    private static void loadIfNotLoaded() throws Exception
+    {
+        if (loaded) return;
+        loaded = true;
+        loadNeededLibraries();
+        int code = jniInit();
+        printIfVerbose("Init code error " + code);
+    }
+
+    private static void loadNeededLibraries() throws Exception
+    {
+        printIfVerbose("Loading needed libraries using directory " + dataDirectoryPath);
+
+        copyResourceIntoDir("/lib/tobii/x64/vcruntime140.dll", dataDirectoryPath);
+        copyResourceIntoDir("/lib/tobii/x64/msvcp140.dll", dataDirectoryPath);
+        copyResourceIntoDir("/lib/tobii/x64/concrt140.dll", dataDirectoryPath);
+        copyResourceIntoDir("/lib/tobii/x64/vccorlib140.dll", dataDirectoryPath);
+        copyResourceIntoDir("/lib/tobii/x64/tobii_stream_engine.dll", dataDirectoryPath);
+        copyResourceIntoDir("/lib/tobii/x64/tobii_jni_stream_engine.dll", dataDirectoryPath);
+
+        // loadLibrary(tmpDir, "/lib/tobii/x64/vcruntime140.dll");
+        // loadLibrary(tmpDir, "/lib/tobii/x64/msvcp140.dll");
+        // loadLibrary(tmpDir, "/lib/tobii/x64/concrt140.dll");
+        // loadLibrary(tmpDir, "/lib/tobii/x64/vccorlib140.dll");
+        // loadLibrary(tmpDir, "/lib/tobii/x64/tobii_stream_engine.dll");
+        loadLibrary(dataDirectoryPath, "/lib/tobii/x64/tobii_jni_stream_engine.dll");
+    }
+
+    private static void copyResourceIntoDir(String resourceFilePath, String dirPath) throws Exception
+    {
+        printIfVerbose("Copying " + resourceFilePath + " into " + dirPath);
+        InputStream in = Tobii.class.getResourceAsStream(resourceFilePath);
+        File tmpFile = new File(dirPath, resourceFilePath);
+        tmpFile.getParentFile().mkdirs();
+        Files.copy(in, tmpFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        in.close();
+    }
+
+    private static void loadLibrary(String dirPath, String filePath)
+    {
+        File tmpFile = new File(dirPath, filePath);
+        printIfVerbose("Loading library " + tmpFile.getAbsolutePath());
+        System.load(tmpFile.getAbsolutePath());
+    }
+
+    private static void printIfVerbose(String what)
+    {
+        if (verbose)
+        {
+            System.out.println("Tobii: " + what);
+        }
+    }
+
+    private static native int jniInit();
+
+    private static native float[] jniGazePosition();
 }
